2772183d9b59a82d0e8013c4f82c0b3fa0253c9f
Aakarshan Sharma <aakarshansharma17@gmail.com>
2024-11-29 23:28:54 -0500

    Create Student Object With Properties

---
+++ Javascript-Lab/script.js
@@ -0,0 +1,10 @@
+const student = {
+    name: 'Alice Johnson',
+    age: 21,
+    enrolled: true,
+    courses: ['Mathematics', 'Physics', 'Chemistry'],
+    displayInfo: function() {
+      return `Student: ${this.name}, Age: ${this.age}`;
+    }
+  };
+  
